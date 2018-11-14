86babc455f2e2d1c235ae0ce50c7d6c5a81d58ab
DESKTOP-HO86IBD\chatu <chaturangarajapakshe@gmail.com>
2018-11-14 20:47:04 +0530

    lab8_oop completed

---
+++ src/pdn/scs/lab8_oop/Scalable.java
@@ -0,0 +1,11 @@
+package pdn.scs.lab8_oop;
+
+/**
+ * The Scalable interface defines methods used in scaling an object.
+ */
+public interface Scalable
+{
+    public void scale(double sf);
+
+    public void scale(double sx, double sy);
+}
